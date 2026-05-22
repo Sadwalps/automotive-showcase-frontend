7a61ecf33580965749401283e6cfb848f9a01859
Sadwalps <sadwalps2255@gmail.com>
2026-05-22 12:15:07 +0530

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from '../components/Header'
+
+function Home() {
+  return (
+    <>
+    <Header/>
+    <div className='text-primary fs-1' style={{}}>sdasd</div>
+    </>
+  )
+}
+
+export default Home
