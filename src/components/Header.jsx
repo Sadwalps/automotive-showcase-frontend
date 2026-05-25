17771790212a013ea6920694168e63bd15f9a650
Sadwalps <sadwalps2255@gmail.com>
2026-05-25 19:17:55 +0530

    second commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,36 @@
 import React from 'react'
-
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Dropdown from 'react-bootstrap/Dropdown';
 function Header() {
   return (
     <>
-    <div>header</div>
+      <div className="" style={{ background: "grey", opacity: "30px" }}>
+        <Navbar expand="lg" className="">
+          <Container>
+            <Navbar.Brand href="#home" className='text-light' style={{ fontFamily: "cursive", fontWeight: "bold" }}>Automotive showcase</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="ms-auto">
+                <div id='navlinks'>Home</div>
+               
+                <Dropdown>
+                  <Dropdown.Toggle   id='navlinks' className='border-0 mt-1 rounded-0' >
+                   Pages
+                  </Dropdown.Toggle>
+
+                  <Dropdown.Menu className='bg-dark'>
+                    <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+                    <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+                    <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+                  </Dropdown.Menu>
+                </Dropdown>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      </div>
     </>
   )
 }
