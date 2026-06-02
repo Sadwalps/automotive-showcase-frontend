9bf3badda948f5667e0917b1f2d0cc36a7b9d4c2
Sadwalps <sadwalps2255@gmail.com>
2026-06-02 19:47:55 +0530

    card

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -54,10 +54,21 @@ function Home() {
             <div className="row">
               <div className="col-md-4 homecarmaincard">
                 <div className='homecarcard' >
-<img src="https://tse3.mm.bing.net/th/id/OIP.l9bZFUj96uX7vw8YQuUcqgHaEK?r=0&rs=1&pid=ImgDetMain&o=7&rm=3" alt="" className='w-100' />
+                  <img src="https://tse3.mm.bing.net/th/id/OIP.l9bZFUj96uX7vw8YQuUcqgHaEK?r=0&rs=1&pid=ImgDetMain&o=7&rm=3" alt="" className='w-100' />
                 </div>
-                <div className='bg-danger homecarhovercard w-100'>
-                   <h2>sdkla</h2>
+                {/* card that appears only when we mouse hover on it */}
+                <div className=' homecarhovercard  '>
+                  <div className='homecarhovercard1'>
+                    <img src="https://ak6.picdn.net/shutterstock/videos/1374016/thumb/6.jpg" alt="" className='w-100' />
+                  </div>
+                  <div className='homecarhovercard2'>
+                     <h3>View car Details</h3>
+                     <div>
+                      <button id='  homecarhovercard2btn'>hi</button>
+                      <button id='  homecarhovercard2btn '>hi</button>
+                     </div>
+                  </div>
+
                 </div>
               </div>
               <div className="col-md-4"></div>
