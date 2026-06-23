b46e98ca9bd5974e5ea26631c672182d5e0172f1
Sadwalps <sadwalps2255@gmail.com>
2026-06-23 19:43:04 +0530

    user role page

--- src/pages/Userrole.jsx
+++ src/pages/Userrole.jsx
@@ -4,29 +4,41 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
 
 function Userrole() {
-  return (
-   <>
-   <div className='userrolepage conatiner-fluid bg-dark'>
-    
-    <div className="row">
-        <div className="col-md-2"></div>
-        <div className="col-md-8 d-flex justify-content-center align-item-center">
-            <button className='d-flex flex-column btn btn-warning p-lg-4 p-2'>
-                <FontAwesomeIcon icon={faBlackTie}  className='fa-6x'/>
-                <h2>Admin</h2>
-            </button>
-             <button className='d-flex flex-column btn btn-info p-lg-4 p-2'>
-                <FontAwesomeIcon icon={faUser} className='fa-6x' />
-                <h2>User</h2>
-            </button>
+    return (
+        <>
 
-        </div>
-        <div className="col-md-2"></div>
-    </div>
-     
-   </div>
-   </>
-  )
+
+
+            <div className=' container-fluid bg-dark'>
+                <div className="row">
+                    <div className="col-md-2"></div>
+
+                    <div className="userrolepage col-md-8 d-flex flex-column justify-content-center align-item-center">
+                        <h1 className='text-light text-center mb-5'>Select Your Role</h1>
+                        <div className='container-fluid'>
+                            <div className="row ">
+                                <div className="col-lg-6 col-md-6 col-sm-6 col-12 d-flex  justify-content-center mt-1"> <button className='d-flex flex-column btn btn-warning p-lg-4 p-2'>
+                                <FontAwesomeIcon icon={faBlackTie} className='fa-6x' />
+                                <h2>Admin</h2>
+                            </button></div>
+                                <div className="col-lg-6 col-md-6 col-sm-6 col-12 d-flex  justify-content-center mt-lg-1 mt-md-1 mt-sm-1 mt-4" > <button className='d-flex flex-column btn btn-info p-lg-4 p-2 '>
+                                <FontAwesomeIcon icon={faUser} className='fa-6x ' />
+                                <h2>User</h2>
+                            </button></div>
+                            </div>
+                           
+                           
+                        </div>
+
+
+                    </div>
+                    <div className="col-md-2"></div>
+                </div>
+
+            </div>
+
+        </>
+    )
 }
 
 export default Userrole
