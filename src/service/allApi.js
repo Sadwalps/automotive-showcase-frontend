9b5ce4d6a7c71572d0223066e05cdfce8be44d02
Sadwalps <sadwalps2255@gmail.com>
2026-06-25 16:21:44 +0530

    service folder

---
+++ src/service/allApi.js
@@ -0,0 +1,7 @@
+import { commonApi } from "./commonApi"
+import { serverUrl } from "./serverUrl"
+
+//Api to add car details
+// export const addCarApi = async (reqBody) => {
+//     return await commonApi(`Post` `${serverUrl}/cars`, reqBody)
+// }
