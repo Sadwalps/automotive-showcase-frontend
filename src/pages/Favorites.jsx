3bcd4435cf114db1283c0a7d3180de55f34424c5
Sadwalps <sadwalps2255@gmail.com>
2026-08-10 15:23:31 +0530

    get liked cars details

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { useState } from 'react';
 import Card from 'react-bootstrap/Card';
 import Modal from 'react-bootstrap/Modal';
@@ -8,7 +8,9 @@ import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faAngleUp } from '@fortawesome/free-solid-svg-icons';
 import Viewimagemodal from '../components/Viewimagemodal';
+import { carContext } from '../context/ContextShare';
 function Favorites() {
+    const { likedcars } = useContext(carContext)
     const [show, setShow] = useState(false);
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
@@ -30,29 +32,34 @@ function Favorites() {
 
             {/* car card's section */}
             <div className='container-fluid pt-lg-5 pt-3'>
-                <div className="row">
+                {likedcars?.length > 0 ? <div className="row">
                     <div className="col-md-1"></div>
                     <div className="col-md-10">
                         <div className="row">
-                            <div className="col-lg-4 col-md-6 col-12">
+                            {likedcars?.map((item) => (<div className="col-lg-4 col-md-6 col-12">
                                 <Card style={{ width: '100%', border: "0px", marginTop: "10px", marginBottom: "20px" }}>
-                                    <Card.Img variant="top" src="https://tse3.mm.bing.net/th/id/OIP.JRE3ZGj58ZPQShbvEq9leAHaEK?r=0&rs=1&pid=ImgDetMain&o=7&rm=3" />
+                                    <Card.Img variant="top" src={item?.imgurl} />
                                     <Card.Body>
-                                        <Card.Title style={{ fontWeight: "bold" }}>Lamborghini centenario</Card.Title>
+                                        <Card.Title style={{ fontWeight: "bold" }}>{item?.carname}</Card.Title>
                                     </Card.Body>
                                     <div className='d-flex justify-content-between'>
-                                        <Viewimagemodal />
+                                        <Viewimagemodal item={item} />
                                         <Link >
                                             <button className='carcardsbtns  px-2 py-1 fs-lg-4  fs-5'>
                                                 <FontAwesomeIcon icon={faAngleUp} />
                                             </button></Link>
                                     </div>
                                 </Card>
-                            </div>
+                            </div>))}
                         </div>
                     </div>
                     <div className="col-md-1"></div>
-                </div>
+                </div> :
+
+                    <div className='d-flex justify-content-center align-items-center' style={{ height: "40vh" }}>
+                        <h1 className='text-primary' style={{ fontWeight: "bold" }} >Page is Empty!!!</h1>
+                    </div>}
+
             </div>
             < Footer />
         </>
